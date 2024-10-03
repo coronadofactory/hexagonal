01e5acb2a1cf65b1e223bc60ea8b458168c1aa72
Software factory <factory@coronadoland.com>
2024-10-03 16:47:48 +0200

    Update IngressProxy.java

--- mesh/IngressProxy.java
+++ mesh/IngressProxy.java
@@ -1,3 +1,13 @@
+/*!
+ * IngressProxy Interface
+ * 
+ * Copyright (c) 1984-2024 Jose Garcia
+ * Released under the MIT license
+ * https://raw.githubusercontent.com/coronadofactory/hexagonal/refs/heads/main/LICENSE.txt
+ *
+ * Date: 2024-10-03
+ */
+
 package com.coronadofactory.mesh;
 
 import com.coronadofactory.cqrs.queries.ProxyException;
