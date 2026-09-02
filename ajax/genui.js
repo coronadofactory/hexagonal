e57d79d45fd56855fbf9245417a5fa8239dd314b
Software factory <factory@coronadoland.com>
2026-09-02 11:02:42 +0200

    Improvments

--- ajax/genui.js
+++ ajax/genui.js
@@ -17,8 +17,8 @@ export class Controller {
         return this;
     }
 
-    api(fetcher) {
-        this.APIFetcher=fetcher;
+    store(fetcher) {
+        this.storeFetcher=fetcher;
         return this;
     }
 
@@ -45,17 +45,26 @@ export class Controller {
     async run() {
 
         const schemaFetcher = this.schemaFetcher?this.schemaFetcher:Promise.resolve({children:[]});
-        const APIFetcher = this.APIFetcher;
+        const storeFetcher = this.storeFetcher?this.storeFetcher:Promise.resolve({});
         const hidrant = this.hidrant;
 
         return new Promise((resolve,reject) => {
             if (this.onlineEvent) window.addEventListener('online', this.onlineEvent);
             if (this.offlineEvent) window.addEventListener('offline', this.offlineEvent);
-            schemaFetcher
-                .then(schema => fill(schema, APIFetcher))
-                .then(schema => Promise.all(schema.children.filter(el => el.template).map(el => render(el.id, el.template, el.props || {}))))
-                .then(() => Promise.all(Array.from(document.querySelectorAll('[data-hidrant]')).map(el => hidrate(el, hidrant))).then(() => this.hidratedEvent?.()))
-                .then(() => resolve())
+
+            Promise.all([schemaFetcher, storeFetcher])
+                .then(([schema, store]) => {
+                    return schema;
+                })
+                .then(schema => {
+                    schema.children.filter(el => el.template && el.id).forEach(el => render(el.id, el.template, el.props || {}))
+                    return schema;
+                })
+                .then(schema => {
+                    schema.children.filter(el => el.hidrant && el.id).forEach(el => hidrate(document.getElementById(el.id), hidrant, el.hidrant))
+                    return schema;
+                })
+                .then(schema => resolve(schema))
                 .catch(err => reject(err))
         })
             
@@ -63,12 +72,11 @@ export class Controller {
 
 }
 
-import { fill } from "./store.js";
 import { render } from "./renderer.js";
 
-async function hidrate(el, hidrate) {
+async function hidrate(el, hidrate, hidrateName) {
     if (!hidrate) return Promise.reject('No hidrant defined');
-    return Promise.resolve(hidrate(el.getAttribute('data-hidrant'), el))
+    return Promise.resolve(hidrate(hidrateName, el))
 }
 
 export default Controller;
